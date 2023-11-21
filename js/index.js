d1386094e2bd661cd9e92ca63dc94962b6033bce
DuarteOnLines <duarteonlines@gmail.com>
2023-11-21 14:21:16 -0300

    button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-$('.slide').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
+const showOnPx = window.innerHeight;
+const voltarAoTopoButton = document.getElementById('voltarAoTopo');
+
+window.addEventListener('scroll', () => {
+ if (window.pageYOffset > showOnPx) {
+   voltarAoTopoButton.classList.remove('hidden');
+ } else {
+   voltarAoTopoButton.classList.add('hidden');
+ }
+});
+
+voltarAoTopoButton.addEventListener('click', () => {
+ window.scrollTo({
+ top: 0,
+ behavior: 'smooth'
+ });
 });
